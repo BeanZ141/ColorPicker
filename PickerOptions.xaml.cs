db87770fe3eb7bd103938eb8cdff572978de0e1c
BeanZ <92383674+BeanZ141@users.noreply.github.com>
2024-09-09 19:21:11 +0530

    Added PickerOptions to repo

---
+++ PickerOptions.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Drawing;
+
+namespace PickerOptions
+{
+    public partial class ColorPickerOptionsWindow : Window
+    {
+        public ColorPickerOptionsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) { Close(); }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e) { WindowState = WindowState.Minimized; }
+
+        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
